52cc1de76871e961292b7dba1e402a26bae1cd10
shadrackongera <shadrack.sme@gmail.com>
2023-02-16 11:56:43 +0300

    seller can create offers

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,9 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+////Global routes
+//Route::resource('offers', \App\Http\Controllers\Seller\OfferController::class);
+
 // Admin Routes
 Route::group(['prefix' => 'admin'], function () {
     Route::get('home', [App\Http\Controllers\Admin\HomeController::class, 'index'])->name('admin.home');
@@ -31,5 +34,7 @@ Route::group(['prefix' => 'admin'], function () {
 // Seller Routes
 Route::group(['prefix' => 'seller'], function () {
     Route::get('home', [App\Http\Controllers\Seller\HomeController::class, 'index'])->name('seller.home');
+    Route::get('create', [\App\Http\Controllers\Seller\OfferController::class, 'create'])->name('offer.create');
+    Route::post('store', [\App\Http\Controllers\Seller\OfferController::class, 'store'])->name('offer.store');
 
 });
