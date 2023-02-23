97d588b5d6d817b7720df83ec675567534870a4f
shadrackongera <shadrack.sme@gmail.com>
2023-02-23 17:21:46 +0300

    offers migration

--- database/migrations/2023_02_16_084733_create_offers_table.php
+++ database/migrations/2023_02_16_084733_create_offers_table.php
@@ -15,6 +15,18 @@ return new class extends Migration
     {
         Schema::create('offers', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id')->default(1);
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('title');
+            $table->string('location');
+            $table->string('house_type');
+            $table->integer('price');
+            $table->string('price_length');
+            $table->string('relationship');
+            $table->string('description');
+            $table->string('contact');
+            $table->string('payment_code');
+            $table->string('house_image');
             $table->timestamps();
         });
     }
